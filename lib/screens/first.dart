dc030105c708b6184d4e6b356cf08ad7b17aea90
Ankan Sikdar <ankansikdar@gmail.com>
2021-09-02 21:36:04 +0530

    Implemented GetBuilder and countController

--- lib/screens/first.dart
+++ lib/screens/first.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:state_management/controllers/countController.dart';
 
 class First extends StatelessWidget {
   const First({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final CountController countController = Get.put(CountController());
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(width: double.infinity),
+          GetBuilder<CountController>(
+            builder: (controller) => Text('Count: ${controller.count}'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          countController.increment();
+        },
+        child: Icon(Icons.plus_one),
+      ),
+    );
   }
 }
